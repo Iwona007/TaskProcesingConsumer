17f38837fca04cb70e57d310b7e00975a9d314d6
Iwona007 <iwonka.adamkiewicz@gmail.com>
2023-03-18 23:00:29 +0100

    commit main class

---
+++ src/main/java/pl/iwona/TaskProcessingConsumer/TaskProcessingConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.iwona.TaskProcessingConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProcessingConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskProcessingConsumerApplication.class, args);
+	}
+
+}
